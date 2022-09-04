6f23fea832a1209b961794f0e3a7717808e095fc
hirosphere-ja <51845520+hirosphere-ja@users.noreply.github.com>
2022-09-04 23:32:07 +0900

    リアルタイム更新から一度だけの読み込みに変更
    データが更新されるとリアルタイムで一覧表示を反映されていたのを一度だけの表示に変更

--- lib/viewmodels/list_model.dart
+++ lib/viewmodels/list_model.dart
@@ -3,26 +3,20 @@ import 'package:flutter/material.dart';
 import 'package:job_change_record/models/company.dart';
 
 class ListModel extends ChangeNotifier {
-  final Stream<QuerySnapshot> _companiesStream =
-      FirebaseFirestore.instance.collection('companies').snapshots();
-
   List<Company>? companies;
 
-  void fetchCompanies() {
-    _companiesStream.listen(
-      (QuerySnapshot snapshot) {
-        final List<Company> companies = snapshot.docs.map(
-          (DocumentSnapshot document) {
-            Map<String, dynamic> data =
-                document.data()! as Map<String, dynamic>;
-            final String companyName = data['company_name'];
-            final Timestamp applicationDate = data['application_date'];
-            return Company(companyName, applicationDate);
-          },
-        ).toList();
-        this.companies = companies;
-        notifyListeners();
+  void fetchCompanies() async {
+    final QuerySnapshot snapshot =
+        await FirebaseFirestore.instance.collection('companies').get();
+    final List<Company> companies = snapshot.docs.map(
+      (DocumentSnapshot document) {
+        Map<String, dynamic> data = document.data() as Map<String, dynamic>;
+        final String companyName = data['company_name'];
+        final Timestamp applicationDate = data['application_date'];
+        return Company(companyName, applicationDate);
       },
-    );
+    ).toList();
+    this.companies = companies;
+    notifyListeners();
   }
 }
