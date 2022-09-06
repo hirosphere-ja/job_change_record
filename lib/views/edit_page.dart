043288c22872d23f39151251f9a3881f4779124c
hirosphere-ja <51845520+hirosphere-ja@users.noreply.github.com>
2022-09-07 00:56:19 +0900

    編集画面に入力フォームとボタンを作成
    会社名のTextFormFieldを追加
    datetime_picker_formfield 2.0.1をインストールして応募日のDateTimeFieldを追加
    新規作成のボタンを追加

--- lib/views/edit_page.dart
+++ lib/views/edit_page.dart
@@ -1,4 +1,6 @@
+import 'package:datetime_picker_formfield/datetime_picker_formfield.dart';
 import 'package:flutter/material.dart';
+import 'package:intl/intl.dart';
 
 class EditPage extends StatelessWidget {
   const EditPage({super.key});
@@ -9,8 +11,36 @@ class EditPage extends StatelessWidget {
       appBar: AppBar(
         title: const Text('編集画面'),
       ),
-      body: const Center(
-        child: Text('edit page'),
+      body: Form(
+        child: Column(
+          children: [
+            TextFormField(
+              decoration: const InputDecoration(
+                labelText: '会社名',
+              ),
+            ),
+            DateTimeField(
+              decoration: const InputDecoration(
+                labelText: '応募日',
+              ),
+              format: DateFormat("yyyy-MM-dd"),
+              onShowPicker: (
+                BuildContext context,
+                DateTime? currentValue,
+              ) {
+                return showDatePicker(
+                    context: context,
+                    firstDate: DateTime(1900),
+                    initialDate: currentValue ?? DateTime.now(),
+                    lastDate: DateTime(2100));
+              },
+            ),
+            ElevatedButton(
+              onPressed: () {},
+              child: const Text('新規作成'),
+            )
+          ],
+        ),
       ),
     );
   }
